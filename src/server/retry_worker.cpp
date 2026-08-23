b09372dab2ad7635294faf8381779b803a3363df
kaherro <torkomyanvv@gmail.com>
2026-08-23 18:25:08 +0300

    [feat]: Added TTL in retry_worker

--- src/server/retry_worker.cpp
+++ src/server/retry_worker.cpp
@@ -3,6 +3,7 @@
 #include <chrono>
 #include <thread>
 #include <iostream>
+#include <ctime>
 
 void start_retrying_worker(db_manager &db, std::atomic<bool> &running) {
     std::cout << "[RETRY_WORKER] Started\n";
@@ -11,11 +12,16 @@ void start_retrying_worker(db_manager &db, std::atomic<bool> &running) {
         if(pending_messages.empty()) {
             std::cout << "[RETRY_WORKER] No pending messages left\n"; 
         }
+        int64_t current_time = static_cast<int64_t>(std::time(nullptr));
         for(auto msg : pending_messages) {
             if(retry_deliver_message(db, msg)) {
                 std::cout << "[RETRY_WORKER] Message " << msg.message_id << " successfully delivered\n"; 
             }
             else {
+                if(current_time - msg.timestamp > 86400) {
+                    db.mark_failed(msg.message_id); 
+                    std::cout << "[RETRY_WORKER] Message " << msg.message_id << " now is nomore in retrying_worker\n"; 
+                }
                 std::cout << "[RETRY_WORKER] " << msg.recipient_id << " is still offline\n"; 
             }
         }
